881c4bb56a342a1cd4f6b512cd2851334d142e00
MIGUEL <mikiserrano2018@gmail.com>
2022-08-19 14:59:10 -0500

    un paso mas

--- mail/contact.php
+++ mail/contact.php
@@ -1,6 +1,32 @@
 //--!contacto--!//
 
-<?php
+<?php 
+
+incluir (" control-group ");
+
+if (isset ( $ _POST [ 'registrar' ])) {
+    if (strlen ( $ _POST [ 'nombre' ]) >= 1 && strlen ( $ _POST [ 'correo electrónico' ]) >= 1 ) {
+	    $ nombre = trim( $ _POST [ 'nombre' ]);
+	    $ correo electrónico = recortar ( $ _POST [ 'correo electrónico' ]);
+	    $ consulta =" INSERT INTO datos(nombre, email, ) VALORES(' $ name ',' $ mail ','description') ";
+	    $ resultado = mysqli_query( $ conex , $ consulta );
+	    si ( $ resultado ) {
+	    	?> 
+	    	< h3  class =" ok " > ¡Te ha enviado correctamente! </ h3 >
+           <?php
+	    } más {
+	    	?> 
+	    	< h3  class =" bad " > ¡Ups ha ocurrido un error! </ h3 >
+           <?php
+	    }
+    }    más {
+	    	?> 
+	    	< h3  class =" bad " > ¡Por favor completa los campos! </ h3 >
+           <?php
+    }
+}
+
+?>
 
 
 
